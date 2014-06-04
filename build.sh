6bb21935b81f51f5a6e5b070d4dc280049df0730
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-06-04 11:05:55 -0600

    build.sh updates

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-/root/dockerfiles/start_scripts/build.sh
+. /root/dockerfiles/start_scripts/build.sh $@ && (echo "Parent build.sh failed"; exit 1)
+
+# Build hwx/hdp_storm_node
+echo -e "\n*** Building hwx/hdp_storm_node ***\n"
+cd /root/$REPO_DIR/dockerfiles/hdp_storm_node
+docker build -t hwx/hdp_storm_node .
+echo -e "\n*** Build of hwx/hdp_storm_node complete! ***\n"
+
+remove_untagged_images.sh
 
 echo -e "\n*** The lab environment has successfully been built for this classroom VM ***\n"
