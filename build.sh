95304ae0192e4733054e47726b5f61b3f8f5fad7
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-06-04 12:49:40 -0600

    Added installation of Gradle and am updated build.gradle file containing Storm and Cascading dependencies

--- build.sh
+++ build.sh
@@ -10,4 +10,17 @@ echo -e "\n*** Build of hwx/hdp_storm_node complete! ***\n"
 
 remove_untagged_images.sh
 
+#Install Gradle and download dependencies
+if [[ ! -d /root/.gvm ]];
+then
+  curl -s get.gvmtool.net | bash
+  source "/root/.gvm/bin/gvm-init.sh"
+  echo "gvm_auto_answer=true" > $GVM_DIR/etc/config
+  echo "gvm_auto_selfupdate=false" >> $GVM_DIR/etc/config
+  gvm install gradle
+  cd /root/$COURSE_DIR/labs
+  gradle dependencies
+fi
+
+
 echo -e "\n*** The lab environment has successfully been built for this classroom VM ***\n"
