5e40d84326e8f90a01ec7c8b5b2d730ff6e75bf6
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-06-04 23:13:11 -0700

    Added dockerfiles for HDP/Storm, HDP/Spark, and both, along with start scripts. Modified build.sh to install Hive

--- build.sh
+++ build.sh
@@ -2,11 +2,26 @@
 
 . /root/dockerfiles/start_scripts/build.sh $@ && (echo "Parent build.sh failed"; exit 1)
 
+apt-get install curl
+apt-get install hive
+
 # Build hwxu/hdp_storm_node
 echo -e "\n*** Building hwux/hdp_storm_node ***\n"
 cd /root/$REPO_DIR/dockerfiles/hdp_storm_node
 docker build -t hwxu/hdp_storm_node .
 echo -e "\n*** Build of hwxu/hdp_storm_node complete! ***\n"
+
+# Build hwxu/hdp_spark_node
+#echo -e "\n*** Building hwux/hdp_spark_node ***\n"
+#cd /root/$REPO_DIR/dockerfiles/hdp_spark_node
+#docker build -t hwxu/hdp_spark_node .
+#echo -e "\n*** Build of hwxu/hdp_spark_node complete! ***\n"
+
+# Build hwxu/hdp_spark_storm_node
+#echo -e "\n*** Building hwux/hdp_spark_storm_node ***\n"
+#cd /root/$REPO_DIR/dockerfiles/hdp_spark_storm_node
+#docker build -t hwxu/hdp_spark_storm_node .
+#echo -e "\n*** Build of hwxu/hdp_spark_storm_node complete! ***\n"
 
 remove_untagged_images.sh
 
@@ -21,6 +36,5 @@ then
   cd /root/$COURSE_DIR/labs
   gradle dependencies
 fi
-
 
 echo -e "\n*** The lab environment has successfully been built for this classroom VM ***\n"
