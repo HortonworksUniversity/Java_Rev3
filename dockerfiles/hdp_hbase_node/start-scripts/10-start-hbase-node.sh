e94971f072d80efd8092528eb13b2921291dc412
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-06-17 18:12:16 -0700

    Added hdp_hbase_node

---
+++ dockerfiles/hdp_hbase_node/start-scripts/10-start-hbase-node.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$NODE_TYPE" == "namenode" ] ; then
+        #Start zookeeper
+        su - zookeeper -c "echo 1 > /hadoop/zookeeper/data/myid"
+        su - zookeeper -c 'source /etc/zookeeper/conf/zookeeper-env.sh ; export ZOOCFGDIR=/etc/zookeeper/conf;/usr/lib/zookeeper/bin/zkServer.sh start >> /var/log/zookeeper/zoo.out 2>&1'
+        /usr/lib/hbase/bin/hbase-daemon.sh start master
+elif [ "$NODE_TYPE" == "resourcemanager" ] ; then
+	# Start zookeeper
+        su - zookeeper -c "echo 2 > /hadoop/zookeeper/data/myid"
+        su - zookeeper -c 'source /etc/zookeeper/conf/zookeeper-env.sh ; export ZOOCFGDIR=/etc/zookeeper/conf;/usr/lib/zookeeper/bin/zkServer.sh start >> /var/log/zookeeper/zoo.out 2>&1'
+elif [[ ("$NODE_TYPE" == "hiveserver") ]]; then
+	# Start zookeeper
+        su - zookeeper -c "echo 3 > /hadoop/zookeeper/data/myid"
+        su - zookeeper -c 'source /etc/zookeeper/conf/zookeeper-env.sh ; export ZOOCFGDIR=/etc/zookeeper/conf;/usr/lib/zookeeper/bin/zkServer.sh start >> /var/log/zookeeper/zoo.out 2>&1'
+elif [[ ("$NODE_TYPE" == "workernode") ]]; then
+	/usr/lib/hbase/bin/hbase-daemon.sh start regionserver
+fi
+
