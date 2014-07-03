6ca1858f50e1bf79dbc8e5d5fe589e9c14e919f2
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-07-02 19:07:13 -0700

    Added Kafka to Storm cluster deployment - 4 Kafka brokers are launched, one on each worker machine

--- dockerfiles/hdp_storm_node/start-scripts/10-start-storm-node.sh
+++ dockerfiles/hdp_storm_node/start-scripts/10-start-storm-node.sh
@@ -14,7 +14,9 @@ elif [[ ("$NODE_TYPE" == "hiveserver") ]]; then
         su - zookeeper -c "echo 3 > /hadoop/zookeeper/data/myid"
         su - zookeeper -c 'source /etc/zookeeper/conf/zookeeper-env.sh ; export ZOOCFGDIR=/etc/zookeeper/conf;/usr/lib/zookeeper/bin/zkServer.sh start >> /var/log/zookeeper/zoo.out 2>&1'
 elif [[ ("$NODE_TYPE" == "workernode") ]]; then
-   cat /root/supervisor-conf/supervisor-supervisord.conf >> /etc/supervisord.conf
+   host=`hostname`
+   ln -s /opt/kafka/config/server-${host}.properties /opt/kafka/config/server.properties
+   cat /root/conf/supervisor/worker-supervisord.conf >> /etc/supervisord.conf
    #sed -i "/.*logviewer.port:.*/c\logviewer.port: $LOG_PORT" /etc/storm/conf/storm.yaml
 fi
 
