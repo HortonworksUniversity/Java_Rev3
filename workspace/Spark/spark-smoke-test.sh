3c494337d71654ebc91b110cf17c830bd6978f64
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-07-17 19:40:43 -0700

    Upgraded Spark to v1.0.1 tech preview from Hortonworks

--- workspace/Spark/spark-smoke-test.sh
+++ workspace/Spark/spark-smoke-test.sh
@@ -2,6 +2,4 @@
 
 cd /usr/lib/spark
 
-SPARK_JAR=lib/spark-assembly_2.10-0.9.1.2.1.1.0-22-hadoop2.4.0.2.1.1.0-385.jar \
-SPARK_YARN_APP_JAR=/usr/lib/spark/examples/lib/spark-examples_2.10-0.9.1.2.1.1.0-22.jar \
-./bin/spark-class org.apache.spark.deploy.yarn.Client --jar examples/lib/spark-examples_2.10-0.9.1.2.1.1.0-22.jar --class org.apache.spark.examples.SparkPi --args yarn-standalone --num-workers 2 --master-memory 512m --worker-memory 512m --worker-cores 1
+./bin/spark-submit --class org.apache.spark.examples.SparkPi --master yarn-client --num-executors 3 --driver-memory 512m --executor-memory 512m --executor-cores 1 lib/spark-examples*.jar 10
