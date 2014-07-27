026b9906856591c6bf624e2ad4810e2a7a8f1936
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-07-26 23:10:36 -0700

    Added Gradle config to compile Scala source in Spark project, and added sample Scala WordCount implementation for Spark

--- workspace/Spark/src/main/java/spark/WordCount.java
+++ workspace/Spark/src/main/java/spark/WordCount.java
@@ -16,25 +16,28 @@ public class WordCount {
   @SuppressWarnings("serial")
   public static void main(String... args) {
     JavaSparkContext sc = new JavaSparkContext("yarn-client", "word-count");
-    JavaRDD<String> file = sc.textFile("hdfs://namenode:8020/user/root/constitution.txt");
+    JavaRDD<String> file = sc
+        .textFile("hdfs://namenode:8020/user/root/constitution.txt");
     JavaRDD<String> words = file.flatMap(new FlatMapFunction<String, String>() {
       public Iterable<String> call(String s) {
         return Arrays.asList(s.split(" "));
       }
     });
-    
-    JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
-      public Tuple2<String, Integer> call(String s) {
-        return new Tuple2<String, Integer>(s, 1);
-      }
-    });
 
-    JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
-      @Override
-      public Integer call(Integer i1, Integer i2) {
-        return i1 + i2;
-      }
-    });
+    JavaPairRDD<String, Integer> pairs = words
+        .mapToPair(new PairFunction<String, String, Integer>() {
+          public Tuple2<String, Integer> call(String s) {
+            return new Tuple2<String, Integer>(s, 1);
+          }
+        });
+
+    JavaPairRDD<String, Integer> counts = pairs
+        .reduceByKey(new Function2<Integer, Integer, Integer>() {
+          @Override
+          public Integer call(Integer i1, Integer i2) {
+            return i1 + i2;
+          }
+        });
 
     counts.saveAsTextFile("hdfs://namenode:8020/user/root/sparkwordcount");
   }
