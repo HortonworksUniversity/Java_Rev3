50080c06cc832c73ff79e8a6baeb0a36e02982e8
rfraposa <rich@hortonworks.com>
2014-09-17 21:05:14 -0600

    MapSideJoin now reads the LocalResource using a FileReader

--- labs/Solutions/Lab7.1/MapSideJoin/src/main/java/mapjoin/MapSideJoin.java
+++ labs/Solutions/Lab7.1/MapSideJoin/src/main/java/mapjoin/MapSideJoin.java
@@ -1,6 +1,7 @@
 package mapjoin;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
@@ -32,11 +33,7 @@ public class MapSideJoin extends Configured implements Tool {
 		@Override
 		protected void setup(Context context) throws IOException, InterruptedException {
 			stockSymbol = context.getConfiguration().get("stockSymbol");
-			Path [] cacheFiles = context.getLocalCacheFiles();
-			if(cacheFiles.length > 0) {
-				FileSystem fs = FileSystem.getLocal(context.getConfiguration());
-				FSDataInputStream is = fs.open(cacheFiles[0]);
-				BufferedReader in = new BufferedReader(new InputStreamReader(is));
+				BufferedReader in = new BufferedReader(new FileReader("NYSE_dividends_A.csv"));
 				String currentLine = "";
 				while((currentLine = in.readLine()) != null) {
 					String [] words = StringUtils.split(currentLine,'\\',',');
@@ -48,7 +45,6 @@ public class MapSideJoin extends Configured implements Tool {
 						stocks.put(stock, dividend);
 					}
 				}
-			}
 		}
 
 		@Override
