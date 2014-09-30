04c2d7defbecbf2d0084a42cd962dbdf20f52c8f
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-09-29 22:44:40 -0700

    Added Scalding example apps, along with a hack to allow Scalding jobs to run on Tez

--- workspace/Cascading/src/main/scala/scalding/WordCount.scala
+++ workspace/Cascading/src/main/scala/scalding/WordCount.scala
@@ -1,10 +1,36 @@
 package scalding
 
 import com.twitter.scalding._
+import cascading.flow.tez.FlowProcessor
 
-class WordCountJob(args : Args) extends Job(args) {
-  TextLine( args("input") )
-    .flatMap('line -> 'word) { line : String => line.split("""\s+""") }
+class WordCountJob(args: Args) extends Job(args) {
+  TextLine(args("input"))
+    .flatMap('line -> 'word) { line: String => line.split("""\s+""") }
     .groupBy('word) { _.size }
-    .write( Tsv( args("output") ) )
+    .write(Tsv(args("output")))
+
+  override def config: Map[AnyRef, AnyRef] = {
+    val config = super.config
+    if (args.boolean("tez")) {
+      config ++ Map("cascading.flow.runtime.gather.partitions.num" -> "4",
+        "tez.lib.uris" -> "hdfs:///apps/tez-0.5.0/tez-0.5.0.tar.gz",
+        "cascading.app.appjar.class" -> this.getClass())
+    } else {
+      config ++ Map("cascading.app.appjar.class" -> this.getClass())
+    }
+  }
+
+  override def run: Boolean = {
+    val flow = buildFlow
+    flow.complete
+
+    if (!args.boolean("tez")) {
+      val statsData = flow.getFlowStats
+      handleStats(statsData)
+      statsData.isSuccessful
+    } else {
+      true
+    }
+  }
 }
+
